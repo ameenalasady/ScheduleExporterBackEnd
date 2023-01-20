2ba15e651586770d14d89467a79b4ae82a935c67
ameenalasady <105679326+ameenalasady@users.noreply.github.com>
2023-01-19 22:31:04 -0500

    Update

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, request, send_file
+from getter import getCSV
 import os
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/csv")
+def returnCSV():
+    username = request.args.get("username")
+    password = request.args.get("password")
+    print(username, password)
+    responseFromMain = getCSV(str(username), str(password))
+    return responseFromMain[0]
 
 
-if __name__ == '__main__':
+@app.route("/download")
+def downloadFile():
+    username = request.args.get("username")
+    password = request.args.get("password")
+    print(username, password)
+    responseFromMain = getCSV(str(username), str(password))
+    filename = responseFromMain[1]+".csv"
+
+    return send_file("C:\\GitHubRepos\\ScheduleExporterWebApp\\flask-server\output\\"+filename, as_attachment=True)
+
+
+if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
