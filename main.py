68fbb5254c80aa671ab34ad751b6840d66a75cfa
ameenalasady <105679326+ameenalasady@users.noreply.github.com>
2023-01-19 23:46:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods',
-                         'GET,PUT,POST,DELETE,OPTIONS')
-    return response
-
-
 @app.route("/csv")
 def returnCSV():
     try:
