0afed060cf639b2bfb0363f6b7e5732df8c1fa6b
ameenalasady <105679326+ameenalasady@users.noreply.github.com>
2023-01-20 19:05:42 -0500

    Encryption

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
 from getter import getCSV
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, asymmetric
 import os
+
+
+# generate private key
+private_key = asymmetric.rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+
+# generate public key
+public_key = private_key.public_key()
+
+# convert private key to pem format
+private_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+
+# convert public key to pem format
+public_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
 
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.route('/public_key', methods=['GET'])
+def get_public_key():
+    return public_pem
 
 
 @app.route("/csv")
